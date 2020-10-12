db642382b2d920f590ce35c95a5f0e15eae65899
drapdrop1 <drapdrop1@gmail.com>
2020-10-12 16:52:06 +0300

    deleted  comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Calc from './pages/index.jsx';
-//import onKey from './scripts/keypress';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
@@ -12,5 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//document.addEventListener('keydown', onKey.bind(Calc));
 serviceWorker.unregister();
