945091bc574411ae57db2b10f6de1fdd3839ef60
drapdrop1 <drapdrop1@gmail.com>
2020-10-12 12:22:27 +0300

    before textfield

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,87 +5,229 @@ import './index.scss';
 class Calc extends Component {
     state = {
         isSecond: false,
-        first: 0,
-        second: 0
+        firsts: 0,
+        seconds: 0,
+        res: 0,
+        isRes: false,
+        isEq: false
+    };
+
+    resToNum = (num) => {
+        const { isEq } = this.state;
+        isEq ? this.setState({
+            isSecond: false,
+            firsts: num,
+            seconds: 0,
+            isRes: false,
+            isEq: false
+        }) : this.setState({
+            seconds: num,
+            isRes: false
+        });
     };
 
     numType = (num) => {
-        const { first, second, isSecond } = this.state;
-        console.log(first, num)
-        isSecond ? this.setState({
-            second : second === 0 && num !== 0 ? '' + num : second + num
-        }) : this.setState({
-            first : first === 0 && num !== 0 ? '' + num : first + num
-        });
+        const { firsts, seconds, isSecond, isRes } = this.state;
+        if (isRes) {
+            this.resToNum(num);
+        }
+        else {
+            isSecond ? this.setState({
+                seconds : seconds == 0 && num != 0 ? `${num}` : `${ seconds }` + num
+            }) : this.setState({
+                firsts : firsts == 0 && num != 0 ? `${num}`: `${ firsts }` + num
+            });
+        }
     };
 
     onCClick = () => {
         this.setState({
-            first: 0,
-            second: 0,
-            isSecond: false
+            firsts: 0,
+            seconds: 0,
+            isSecond: false,
+            res: 0,
+            isRes: false,
+            isEq: false
         });
     };
 
     onDotClick = () => {
-        const { isSecond, first, second} = this.state;
-        isSecond && !('' + second).includes('.') ? this.state({
-            second: second + '.'
-        }) : !('' + first).includes('.') && this.setState({
-            first: first + '.'
+        const { isSecond, firsts, seconds} = this.state;
+        isSecond && !('' + seconds).includes('.') ? this.setState({
+            seconds: seconds + '.'
+        }) : !('' + firsts).includes('.') && this.setState({
+            firsts: firsts + '.'
         });
     };
 
     onPMClick = () => {
-        const { first, second, isSecond } = this.state;
-        if (isSecond && second !== 0) {
+        const { firsts, seconds, isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onCClick();
             this.setState({
-                second: +second < 0 ? second.slice(1,) : '-' + second
+                firsts: +seconds < 0 ? seconds.slice(1,) : '-' + seconds,
+                isRes: true,
+                isEq: true
             });
         }
-        if (!isSecond && first !== 0) {
-            this.setState({
-                first: +first < 0 ? first.slice(1,) : '-' + first
-            });
+        else {
+            if (isSecond && seconds !== 0) {
+                this.setState({
+                    seconds: +seconds < 0 ? seconds.slice(1,) : '-' + seconds
+                });
+            }
+            if (!isSecond && firsts !== 0) {
+                this.setState({
+                    firsts: +firsts < 0 ? firsts.slice(1,) : '-' + firsts
+                });
+            }
         }
     }
 
     onCEClick = () => {
-        const { isSecond } = this.state;
-        isSecond ? this.setState({
-            second: 0
-        }) : this.setState({
-            first: 0
-        });
+        const { isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onCClick();
+        }
+        else {
+            isSecond ? this.setState({
+                seconds: 0,
+                res: 0,
+                isRes: false
+            }) : this.setState({
+                firsts: 0,
+                res: 0,
+                isRes: false
+            });
+        }
     };
 
     onDClick = () => {
-        const { first, second, isSecond } = this.state;
-        if (isSecond && second !== 0) {
+        const { firsts, seconds, isSecond } = this.state;
+        if (isSecond && seconds !== 0) {
             this.setState({
-                second: second[0] === '-' && second.length === 2 || second === '-0.' ? 0 : 
-                    (second.length > 1 ? second.slice(0, second.length - 1) : 0)
+                seconds: seconds[0] === '-' && seconds.length === 2 || seconds === '-0.' ? 0 : 
+                    (seconds.length > 1 ? seconds.slice(0, seconds.length - 1) : 0)
             });
         }
-        if (!isSecond && first !== 0) {
+        if (!isSecond && firsts !== 0) {
             this.setState({
-                first: first[0] === '-' && first.length === 2 || first === '-0.' ? 0 : 
-                    (first.length > 1 ? first.slice(0, first.length - 1) : 0)
+                firsts: firsts[0] === '-' && firsts.length === 2 || firsts === '-0.' ? 0 : 
+                    (firsts.length > 1 ? firsts.slice(0, firsts.length - 1) : 0)
             });
         }
     };
 
+    calcRes = () => {
+        const { firsts, seconds, res } = this.state;
+        switch (firsts[firsts.length - 1]) {
+            case '+':
+                return res + +seconds;
+            case '-':
+                return res - +seconds;
+            case '*':
+                return res * +seconds;
+            case '/':
+                return res / +seconds;
+        }
+    };
+
+    onResSignClick = (sign) => {
+        const { firsts } = this.state;
+        this.setState({
+            firsts: firsts.slice(0, firsts.length - 2) + sign,
+            isEq: false,
+            isSecond: true
+        });
+    };
+
+    onDelClick = () => {
+        const { firsts, seconds, isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onResSignClick('/');
+        }
+        else {
+            this.setState({
+                isSecond: true,
+                firsts: isSecond ? (seconds != 0 ? firsts + ` ${ seconds } /` : firsts) : firsts + ' /',
+                seconds: isSecond ? this.calcRes() : +firsts,
+                res: isSecond ? this.calcRes() : +firsts,
+                isRes: true
+            });
+        }
+    };
+
+    onPlusClick = () => {
+        const { firsts, seconds, isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onResSignClick('+');
+        }
+        else {
+            this.setState({
+                isSecond: true,
+                firsts: isSecond ? (seconds != 0 ? firsts + ` ${ seconds } +` : firsts) : firsts + ' +',
+                seconds: isSecond ? this.calcRes() : +firsts,
+                res: isSecond ? this.calcRes() : +firsts,
+                isRes: true
+            });
+        }
+    };
+
+    onMinClick = () => {
+        const { firsts, seconds, isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onResSignClick('-');
+        }
+        else {
+            this.setState({
+                isSecond: true,
+                firsts: isSecond ? (seconds != 0 ? firsts + ` ${ seconds } -` : firsts) : firsts + ' -',
+                seconds: isSecond ? this.calcRes() : +firsts,
+                res: isSecond ? this.calcRes() : +firsts,
+                isRes: true
+            });
+        }
+    };
+
+    onMulClick = () => {
+        const { firsts, seconds, isSecond, isEq } = this.state;
+        if (isEq) {
+            this.onResSignClick('*');
+        }
+        else {
+            this.setState({
+                isSecond: true,
+                firsts: isSecond ? (seconds != 0 ? firsts + ` ${ seconds } *` : firsts) : firsts + ' *',
+                seconds: isSecond ? this.calcRes() : firsts,
+                res: isSecond ? this.calcRes() : +firsts,
+                isRes: true
+            });
+        }
+    };
+
+    onEqClick = () => {
+        const { isSecond, firsts, seconds } = this.state;
+        this.setState({
+            isSecond: true,
+            firsts: isSecond ? `${ firsts } ${ seconds } =` : `${ firsts } =`,
+            seconds: isSecond ? this.calcRes() : firsts,
+            isRes: true,
+            res: isSecond ? this.calcRes() : +firsts,
+            isEq: true
+        })
+    };
+
     render() {
-        const { isSecond, first, second } = this.state;
+        const { isSecond, firsts, seconds } = this.state;
         return (
             <div className="calc">
                 <div className="top">
                     {
                         isSecond && <div>
-                            <h1 className="tFrst">{ first }</h1>
-                            <h1 className="bScnd">{ second }</h1>
+                            <h1 className="tFrst">{ firsts }</h1>
+                            <h1 className="bScnd">{ seconds }</h1>
                         </div> ||
-                        <h1 className="first">{ first }</h1>
+                        <h1 className="first">{ firsts }</h1>
                     }
                 </div>
                 <div className="bttns">
@@ -103,11 +245,11 @@ class Calc extends Component {
                         <Button onClick={ this.onDotClick }>.</Button>
                     </div>
                     <div className="rightBlock">
-                        <Button>/</Button>
-                        <Button>*</Button>
-                        <Button>-</Button>
-                        <Button>+</Button>
-                        <Button>=</Button>
+                        <Button onClick={ this.onDelClick }>/</Button>
+                        <Button onClick={ this.onMulClick }>*</Button>
+                        <Button onClick={ this.onMinClick }>-</Button>
+                        <Button onClick={ this.onPlusClick }>+</Button>
+                        <Button onClick={ this.onEqClick }>=</Button>
                     </div>
                 </div>
             </div>
